a09675b29942cfc12220de0e43de2d49732800e1
Christian Spielberger <c.spielberger@commend.com>
2023-07-20 11:35:59 +0200

    fix gcc warning; comment format

--- src/wm8960.c
+++ src/wm8960.c
@@ -5,18 +5,21 @@
 #include <zephyr/kernel.h>
 
 static const uint8_t wm8960_init_seq1[][2] = {
-	WM8960_PAIR(WM8960_RESET, 0),     /* Reset to defaults */
-	WM8960_PAIR(WM8960_PWR1, 0x00FE), /* Everything on (vmid, vref, ain, adc, micb, digenb) */
-	WM8960_PAIR(WM8960_PWR2, 0x01FF), /* Everything on (dac, spk, out3, pll) */
-	WM8960_PAIR(WM8960_PWR3, 0x001F), /* Everything on (mixers/mics) */
-	WM8960_PAIR(WM8960_IFACE2,0x0040), /* Slave mode, Word Length 16 bits, Format I2S */
-	WM8960_PAIR(WM8960_LOUT1, 0x016f), /* 0dB */
-	WM8960_PAIR(WM8960_ROUT1, 0x016f), /* 0dB */
-	WM8960_PAIR(WM8960_LMIX, 0x0100), /* DAC routing */
-	WM8960_PAIR(WM8960_RMIX, 0x0100), /* DAC routing */
-	//WM8960_PAIR(WM8960_CTL2, 0x0004), /* Unmute ramp volume */
-	WM8960_PAIR(WM8960_CTL1, 0x0000), /* Unmute */
-	WM8960_PAIR(WM8960_CLASSD1, 0x00F7), /* enable class d */
+	WM8960_PAIR(WM8960_RESET, 0),      /**< Reset to defaults */
+	WM8960_PAIR(WM8960_PWR1, 0x00FE),  /**< Everything on (vmid, vref, ain,
+					        adc, micb, digenb) */
+	WM8960_PAIR(WM8960_PWR2, 0x01FF),  /**< Everything on (dac, spk, out3,
+					       pll) */
+	WM8960_PAIR(WM8960_PWR3, 0x001F),  /**< Everything on (mixers/mics) */
+	WM8960_PAIR(WM8960_IFACE2,0x0040), /**< Slave mode, 16 bits,
+					        format I2S */
+	WM8960_PAIR(WM8960_LOUT1, 0x016f), /**< 0dB */
+	WM8960_PAIR(WM8960_ROUT1, 0x016f), /**< 0dB */
+	WM8960_PAIR(WM8960_LMIX, 0x0100),  /**< DAC routing */
+	WM8960_PAIR(WM8960_RMIX, 0x0100),  /**< DAC routing */
+	//WM8960_PAIR(WM8960_CTL2, 0x0004),  /**< Unmute ramp volume */
+	WM8960_PAIR(WM8960_CTL1, 0x0000),  /**< Unmute */
+	WM8960_PAIR(WM8960_CLASSD1, 0x00F7), /**< enable class d  */
 	WM8960_PAIR(WM8960_IFACE1,0x0002),
 	WM8960_PAIR(WM8960_ADCL, 0x01B8),
 	WM8960_PAIR(WM8960_LIN, 0x0117),
@@ -47,6 +50,8 @@ void wm8960_configure(struct rtio_wm8960 *wm8960,
 		      const struct device *i2s)
 {
 	int ret;
+	const struct i2s_config* i2s_cfg1;
+	struct i2s_config i2s_cfg;
 
 	wm8960->i2c = i2c;
 	wm8960->i2s = i2s;
@@ -54,17 +59,17 @@ void wm8960_configure(struct rtio_wm8960 *wm8960,
 	printk("info name: %s\n", i2c->name);
 
 	for(int i = 0; i < ARRAY_SIZE(wm8960_init_seq1); i++) {
-		ret = i2c_reg_write_byte(wm8960->i2c, WM8960_ADDR, wm8960_init_seq1[i][0], wm8960_init_seq1[i][1]);
-		printk("seq: %hhx, %hhx\n", wm8960_init_seq1[i][0], wm8960_init_seq1[i][1]);
+		ret = i2c_reg_write_byte(wm8960->i2c, WM8960_ADDR,
+					 wm8960_init_seq1[i][0],
+					 wm8960_init_seq1[i][1]);
+		printk("seq: %hhx, %hhx\n", wm8960_init_seq1[i][0],
+		       wm8960_init_seq1[i][1]);
 		if (ret < 0) {
-			printk("Initialization step %d failed with error %d\n", i, ret);
+			printk("Initialization step %d failed with error %d\n",
+			       i, ret);
 		}
 		__ASSERT(ret == 0, "Failed to write out wm8960 init sequence");
 	}
-
-	
-
-	struct i2s_config i2s_cfg;
 
 	i2s_cfg.word_size = 16U;
 	i2s_cfg.channels = 2U;
@@ -77,8 +82,7 @@ void wm8960_configure(struct rtio_wm8960 *wm8960,
 
 	ret = i2s_configure(wm8960->i2s, I2S_DIR_TX, &i2s_cfg);
 
-	struct i2s_config* i2s_cfg1 = i2s_config_get(wm8960->i2s, I2S_DIR_TX);
-
+	i2s_cfg1 = i2s_config_get(wm8960->i2s, I2S_DIR_TX);
 	printk("word_size %d\n", i2s_cfg1->word_size);
 	printk("channels %d\n", i2s_cfg1->channels);
 	printk("frame_clk_freq %d\n", i2s_cfg1->frame_clk_freq);
